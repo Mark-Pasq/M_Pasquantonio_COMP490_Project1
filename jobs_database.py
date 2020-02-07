76c7aa5bfcadf1f660903b524f62e65dbc53150c
Mark-Pasq <pasquantonio_mark_sr@outlook.com>
2020-02-07 18:39:56 -0500

    Updated jobs_database.py

--- jobs_database.py
+++ jobs_database.py
@@ -7,42 +7,42 @@
 
 import sqlite3
 from sqlite3 import Error
-
 global connection
 
 
-def create_connection():
-    global connection
+def create_connection(db):
+    # global connection
     db_file = "test/jobs.db"
     """ create a database connection to the SQLite database
         specified by db_file
     :param db_file: database file
     :return: Connection object or None
     """
+    conn = None
     try:
-        connection = sqlite3.connect(db_file)
-        return connection
+        conn = sqlite3.connect(db_file)
+        return conn
     except Error as e:
         print(e)
 
-    return connection
+    return conn
 
 
-def create_table(con, create_table_sql):
+def create_table(conn, create_table_sql):
     """ create a table from the create_table_sql statement
-    :param con: Connection object
+    :param conn:
     :param create_table_sql: a CREATE TABLE statement
     :return:
     """
     try:
-        c = con.cursor()
+        c = conn.cursor()
         c.execute(create_table_sql)
     except Error as e:
         print(e)
 
 
-
 def main():
+    project_db = r'C:\Mark Pasquantonio\PycharmProjects\M_Pasquantonio_COMP490_Project1\test\job.db'
     sql_create_jobs_table = """CREATE TABLE IF NOT EXISTS Jobs_Listing (
                                     id integer NOT NULL,
                                     type text NOT NULL,
@@ -56,7 +56,7 @@ def main():
                                     );"""
 
     # create a database connection
-    connect = create_connection()
+    connect = create_connection(project_db)
 
     # create tables
     if connect is not None:
