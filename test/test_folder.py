b940515b0829a0451ed7fe4b0ec7cf7cda22e108
Mark-Pasq <pasquantonio_mark_sr@outlook.com>
2020-02-07 14:45:08 -0500

    Added content to test_folder.py

---
+++ test/test_folder.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+# Mark Pasquantonio
+# Senior Design and Dev COMP490
+# Project 1 JobsAssignment Sprint 2
+# Filename: test_jobs.py
+
+import pytest
+import jobs
+import sqlite3
+
+
+@pytest.fixture
+def get_data():
+    import jobs
+    return jobs.get_github_jobs_data()
+
+
+def test_my_jobs_dict(get_data):
+    # first required test
+    assert len(get_data) >= 100
+    assert type(get_data[1]) is dict
+
+
+def test_my_jobs_data(get_data):
+    # any real data should have both full time and Contract
+    # jobs in the list, assert this
+    data = get_data
+    full_time_found = False
+    contract_found = False
+    for item in data:
+        if item['type'] == 'Contract':
+            contract_found = True
+        elif item['type'] == 'Full Time':
+            full_time_found = True
+    assert contract_found and full_time_found
+
+
+def test_my_save_data_func():
+    # second required test
+    demo_data = {'id': 1234, 'type': "Testable"}
+    list_data = []
+    list_data.append(demo_data)
+    file_name = "testfile.txt"
+    jobs.save_data(file_name)
+    test_data = open(file_name, 'r')
+    saved_data = test_data.readlines()
+    # the save puts a newline at the end
+    assert f"{str(demo_data)}\n" in saved_data
+
+
+def test_check_if_db_exists():
+    conn = sqlite3.connect('jobs.db')
+    c = conn.cursor()
+
+    # get the count of tables with the name
+    c.execute(''' SELECT count(name) FROM main.sqlite_master WHERE type='table' AND name='Jobs_Listing' ''')
+
+    # if the count is 1, then table exists
+    if c.fetchone()[0] == 1:
+        # print(f"DEBUG!!!!!!!!!:::::: {c.fetchall()}")
+
+        print('The table exists.')
+    else:
+        print('The table does not exist.')
+
+    # commit the changes to db
+    conn.commit()
+    # close the connection
+    conn.close()
