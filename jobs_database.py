b2a7e2a3442c5b727ea46c47f15ba2831bf92938
Mark-Pasq <pasquantonio_mark_sr@outlook.com>
2020-02-07 20:23:41 -0500

    Updated jobs_database.py

--- jobs_database.py
+++ jobs_database.py
@@ -6,66 +6,30 @@
 # Filename: jobs_database.py
 
 import sqlite3
-from sqlite3 import Error
-global connection
 
 
-def create_connection(db):
-    # global connection
-    db_file = "test/jobs.db"
-    """ create a database connection to the SQLite database
-        specified by db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Error as e:
-        print(e)
+def create_a_table():
+    connection = sqlite3.connect('jobs.db')
+    cursor_obj = connection.cursor()
 
-    return conn
+    # Create table
+    cursor_obj.execute('''CREATE TABLE IF NOT EXISTS Jobs_Listing
+                 (Id integer, Type text, url text, Created_at text, Company text, Company_url text, Location text, 
+                 Title text, Description text)''')
 
+    # Insert a row of data
+    # cursor_obj.execute('''INSERT INTO Jobs_Listing VALUES ('1001', 'Full Time', 'http://www.google.com', '02-07-2020',
+    # 'Microsoft', 'http://www.google.com', 'Seattle', 'Engineer', 'A great job at a great
+    # place!')''')
 
-def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
-    :param conn:
-    :param create_table_sql: a CREATE TABLE statement
-    :return:
-    """
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
+    # Save (commit) the changes
+    connection.commit()
+
+    # We can also close the connection if we are done with it.
+    # Just be sure any changes have been committed or they will be lost.
+    connection.close()
 
 
-def main():
-    project_db = r'C:\Mark Pasquantonio\PycharmProjects\M_Pasquantonio_COMP490_Project1\test\jobs.db'
-    sql_create_jobs_table = """CREATE TABLE IF NOT EXISTS Jobs_Listing (
-                                    id integer NOT NULL,
-                                    type text NOT NULL,
-                                    url text NOT NULL,
-                                    created_at integer NOT NULL,
-                                    company integer NOT NULL,
-                                    company_url text NOT NULL,
-                                    title text NOT NULL,
-                                    location text NOT NULL,
-                                    description text NOT NULL
-                                    );"""
-
-    # create a database connection
-    connect = create_connection(project_db)
-
-    # create tables
-    if connect is not None:
-        # create projects table
-        create_table(connect, sql_create_jobs_table)
-
-    else:
-        print("Error! cannot create the database connection.")
-
-
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+# main()
+create_a_table()
