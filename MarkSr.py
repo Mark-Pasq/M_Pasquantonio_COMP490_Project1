08623f4862fe582047e75d7ecfc995f65813127d
Mark P Pasquantonio Sr <mpasquantoni@student.bridgew.edu>
2020-02-12 12:35:13 -0500

    Updated files

--- MarkSr.py
+++ MarkSr.py
@@ -4,14 +4,14 @@ from sqlite3 import Error
 
 def sql_connection():
     try:
-        conn = sqlite3.connect('mark_sr.sqlite')
+        conn = sqlite3.connect('mark.sqlite')
         return conn
     except Error:
         print(Error)
 
 
 def sql_table():
-    conn = sqlite3.connect('mark_sr.sqlite')
+    conn = sqlite3.connect('mark.sqlite')
     cursor_obj = conn.cursor()
     cursor_obj.execute(
         'CREATE TABLE IF NOT EXISTS ringo(id integer PRIMARY KEY, name text, salary real, '
@@ -20,21 +20,22 @@ def sql_table():
     conn.commit()
 
 
-def count_number_of_rows():
-    pass
+# def count_number_of__rows():
+#     conn = sqlite3.connect('mark.sqlite')
+#     cursor_obj = conn.cursor()
+#     cursor_obj.execute('''SELECT count(*) from ringo''')
+#     rowcount = cursor_obj.fetchall()[0]
+#     print(rowcount)
 
 
-def count_number_of__rows():
-    conn = sqlite3.connect('mark_sr.sqlite')
-    cursor_obj = conn.cursor()
-    cursor_obj.execute('''SELECT count(*) from ringo''')
-    rowcount = cursor_obj.fetchall()[0]
-    print(rowcount)
+# def count_number_of_rows():
+#     pass
 
-    def main():
-        sql_connection()
-        sql_table()
-        count_number_of_rows()
 
-        if __name__ == '__main__':
-            main()
+def main():
+    sql_connection()
+    sql_table()
+    # count_number_of_rows()
+
+    if __name__ == '__main__':
+        main()
